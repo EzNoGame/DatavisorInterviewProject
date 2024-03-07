1c002e577ab92ae4f8008315c8a2da3fa456a58b
Ed.Zhang <71359408+EzNoGame@users.noreply.github.com>
2024-03-07 10:56:43 -0500

    first commit

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,10 +1,16 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace TestPj;
 
 public class Tests
 {
+    private IWebDriver _driver;
     [SetUp]
     public void Setup()
     {
+        _driver = new ChromeDriver();
+        _driver.Navigate().GoToUrl("https://bugbug.io/v2");
     }
 
     [Test]
