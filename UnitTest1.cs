9f3f090885dd964fae6501e9c6c487fd7aa1fa23
Ed.Zhang <71359408+EzNoGame@users.noreply.github.com>
2024-03-07 15:35:02 -0500

    created create test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,5 +1,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 
 namespace TestPj;
@@ -7,7 +8,7 @@ namespace TestPj;
 public class Tests
 {
     private IWebDriver _driver;
-    [OneTimeSetUp]
+    [SetUp]
     public void Setup()
     {
         _driver = new ChromeDriver();
@@ -30,9 +31,8 @@ public class Tests
     public void CreateProject()
     {
         String ProjectName = "Test Project";
-
         //wait for the page to be fully loaded
-        WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+        WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
         try
         {
             wait.Until(c => c.FindElement(By.CssSelector("#app > div > div.sc-gytJtb.hDufxA > div > div > div > div.sc-NsUQg.cZERUX > div > button"))); 
@@ -41,7 +41,6 @@ public class Tests
         }
         catch(Exception)
         {
-            Assert.Fail(message: "timeout");
         }
         
         //get textfield and create button
@@ -80,11 +79,105 @@ public class Tests
     }
 
     [Test]
-    public void Create
+    public void CreateTest()
+    {
+        //wait for the page to be fully loaded
+        WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+       
+        EnterFirstProject();
+        IWebElement newTestButton;
+        try
+        {
+            wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+            wait.Until(c => c.FindElement(By.CssSelector("#app > div > div.sc-gytJtb.hDufxA > section > div > div > div > div > div.sc-fifgRP.loMJFf > header > div > div.sc-bCosJw.LHlLZ > div > button")));
+            newTestButton = _driver.FindElement(By.CssSelector("#app > div > div.sc-gytJtb.hDufxA > section > div > div > div > div > div.sc-fifgRP.loMJFf > header > div > div.sc-bCosJw.LHlLZ > div > button"));
+            newTestButton.Click();
+        }
+        catch(Exception)
+        {
+        }
+
+        Random random = new Random();
+        String testName = "Test "+ random.Next();
+        var name = _driver.FindElement(By.Name("name"));
+        name.SendKeys(testName);
+        var create = _driver.FindElement(By.CssSelector("#app > div > div.sc-gytJtb.hDufxA > div > div > form > div.sc-hIUJlX.eyypRo > button.sc-tagGq.fxwkFr.sc-fXSgeo.cLzWpS"));
+        create.Click();
+
+        try
+        {
+            wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+            wait.Until(c => c.FindElement(By.CssSelector("#app > div > div.sc-gytJtb.hDufxA > div > div > div")));
+           _driver.Navigate().Back();
+        }
+        catch(Exception)
+        {
+            Assert.Fail(message: "timeout");
+        }
+
+       try
+        {
+            wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+            wait.Until(c => c.FindElement(By.CssSelector("#app > div > div.sc-gytJtb.hDufxA > section > div > div > div > div > div.sc-fifgRP.loMJFf > header > div > div.sc-bCosJw.LHlLZ > div > button")));
+            var x = _driver.FindElement(By.CssSelector("#app > div > div.sc-gytJtb.hDufxA > div > div > button"));
+            x.Click();
+        }
+        catch(Exception)
+        {
+        }
+
+        String titleName = "";
+        try
+        {
+            wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+            wait.Until(c => c.FindElement(By.CssSelector("#app > div > div.sc-gytJtb.hDufxA > section > div > div > div > div > div.sc-jiDjCn.dyxyMx > div.sc-cbZHsQ.UhcVs > div > div > div > a:nth-child(1) > div:nth-child(2) > span")));
+            var title = _driver.FindElement(By.CssSelector("#app > div > div.sc-gytJtb.hDufxA > section > div > div > div > div > div.sc-jiDjCn.dyxyMx > div.sc-cbZHsQ.UhcVs > div > div > div > a:nth-child(1) > div:nth-child(2) > span"));
+            titleName = title.Text;
+        }
+        catch(Exception)
+        {
+        }
+
+        Assert.AreEqual(titleName, testName);
+    }
+
+    //Notice: this test should only be tested after CreateTest is done without an error
+    [Test]
+    public void DeleteTest()
+    {
+        CreateTest();
+        var option = _driver.FindElement(By.CssSelector("#app > div > div.sc-gytJtb.hDufxA > section > div > div > div > div > div.sc-jiDjCn.dyxyMx > div.sc-cbZHsQ.UhcVs > div > div > div > a:nth-child(1) > div:nth-child(4) > div > div:nth-child(2) > div > button"));
+        Actions builder = new Actions(_driver);
+        builder.MoveToElement(option).Perform();
+        WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(1));
+        option.Click();
+
+    }
 
     [TearDown]
     public void TearDown()
     {
-        _driver.Quit();
+        // _driver.Quit();
+    }
+
+    private void EnterFirstProject()
+    {
+        WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+        try
+        {
+            wait.Until(c => c.FindElement(By.CssSelector("#app > div > div.sc-gytJtb.hDufxA > div > div > div > div.sc-NsUQg.cZERUX > div > button"))); 
+        }
+        catch(Exception)
+        {
+            Assert.Fail(message: "timeout");
+        }
+        try
+        {
+            var project = _driver.FindElement(By.CssSelector("#app > div > div.sc-gytJtb.hDufxA > div > div > div > div.sc-NsUQg.cZERUX > div > a:nth-child(2)"));
+            project.Click();
+        }
+        catch(Exception)
+        {
+        }
     }
 }
